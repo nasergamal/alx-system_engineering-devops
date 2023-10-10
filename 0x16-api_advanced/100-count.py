05f80da5b1a016a437f5c0a4ad43ca20d67772ee
nasergamal <naser.gamal98@gmail.com>
2023-10-10 21:51:38 +0300

    sort

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -25,7 +25,7 @@ def count_words(subreddit, word_list, word_count={}, after=""):
         else:
             for k, v in word_list.items():
                 word_list[k] = v * word_count[k]
-            s_word_list = dict(sorted(word_list.items(), key=lambda x: x[1],
-                               reverse=True))
+            s_word_list = dict(sorted(word_list.items(),
+                               key=lambda x: (-x[1], x[0])))
             for k, v in s_word_list.items():
                 print(f'{k}: {v}')
